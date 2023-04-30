d898cc98f07c552ccb4f0c30c9c33133f89ec4f3
deargoth <viniciuseugeniovhe@gmail.com>
2023-04-30 16:20:34 -0300

    Translated for english

--- templates/static/site_messages.py
+++ templates/static/site_messages.py
@@ -1,13 +1,15 @@
 # Error messages to all the site
 
 error = {
-    'recipe_not_found': 'Receita não encontrada',
-    'category_not_found': 'Nenhuma categoria com este nome foi encontrada',
-    'already_logged': 'Ops! Você já está logado em uma conta'
+    'recipe_not_found': 'Recipe not found',
+    'category_not_found': 'No category with this name was founded',
+    'already_logged': "Ops! You're already logged",
+    'wrong_credentials': 'Your e-mail or password are wrong. Fix them and try again',
 }
 
 # Success messages to all the site
 success = {
-    'register_done': 'Você foi registrado com sucesso! Agora entre em sua conta',
-    'logout_done': 'Você foi desconectado de sua conta com sucesso',
+    'register_done': 'You have been registered successfully! Now login in your account',
+    'successful_login': 'You have been login successfully, enjoy our site!',
+    'logout_done': 'You have been successfully disconnected from your account',
 }
