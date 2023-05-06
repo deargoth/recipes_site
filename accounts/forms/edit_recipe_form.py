4b9c47d52c52864a1461b31504c6091ede569fb0
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-06 19:53:21 -0300

    Adicionando widgets e attrs para os campos

--- accounts/forms/edit_recipe_form.py
+++ accounts/forms/edit_recipe_form.py
@@ -5,18 +5,36 @@ from utils.functions import add_attr
 
 
 class EditRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-    preparation_time_unit = forms.CharField(
-        error_messages={'required': 'This field cannot be empty'},
-        help_text='Ex.: Hours, minutes, days',
-    )
-    servings_unit = forms.CharField(
-        error_messages={'required': 'This field cannot be empty'},
-        help_text='Ex.: People, person',
-    )
+        add_attr(self.fields['preparation_steps'], 'class', 'span-2')
 
     class Meta:
         model = Recipe
         fields = '__all__'
         exclude = ('author', 'is_published',
                    'preparation_steps_is_html', 'category')
+        widgets = {
+            'servings_unit': forms.Select(
+                attrs={
+                    'class': 'span-2'
+                },
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pessoas', 'Pessoas'),
+                    ('Pedaços', 'Pedaços'),
+                )
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                )
+            ),
+            'image': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+        }
