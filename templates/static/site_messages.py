1812b4ea417b2f0d29acc5c644b7c71e35739d08
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-06 17:34:28 -0300

    feat: Mensagem 'permission_required'

--- templates/static/site_messages.py
+++ templates/static/site_messages.py
@@ -1,16 +1,18 @@
 # Error messages to all the site
 
 error = {
-    'recipe_not_found': 'Recipe not found',
-    'category_not_found': 'No category with this name was founded',
-    'already_logged': "Ops! You're already logged",
+    'recipe_not_found': 'Recipe not found.',
+    'category_not_found': 'No category with this name was founded.',
+    'already_logged': "Ops! You're already logged.",
     'login_required': "Ops! You need to be logged to do this!",
-    'wrong_credentials': 'Your e-mail or password are wrong. Fix them and try again',
+    'wrong_credentials': 'Your e-mail or password are wrong. Fix them and try again.',
+    'permission_required': "Ops! You don't have permission to see that."
 }
 
 # Success messages to all the site
 success = {
-    'register_done': 'You have been registered successfully! Now login in your account',
+    'register_done': 'You have been registered successfully! Now login in your account.',
     'successful_login': 'You have been login successfully, enjoy our site!',
-    'logout_done': 'You have been successfully disconnected from your account',
+    'logout_done': 'You have been successfully disconnected from your account.',
+    'recipe_updated': 'Your recipe was edited successfully!'
 }
