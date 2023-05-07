d2dd958e8d01f0b7a7f21ec9d5614da1aea5aa5b
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-07 17:17:29 -0300

    Urls da API que retorna a página em Json

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,8 +5,10 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.Index.as_view(), name="index"),
+    path('recipes/api/v1/', views.IndexApi.as_view(), name="index_api"),
     path('recipes/<str:category_name>',
          views.CategoriesPage.as_view(), name="category"),
     path('details/<int:pk>', views.Details.as_view(), name="details"),
+    path('details/api/v1/<int:pk>', views.DetailsApi.as_view(), name="details_api"),
     path('recipes/search/', views.Search.as_view(), name="search"),
 ]
