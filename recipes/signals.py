abca2876241932f87ef2a4f9ffbfae62b905ffa6
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-15 18:07:47 -0300

    Adicionando um signals para apagar a imagem que não está sendo utilizada mais

---
+++ recipes/signals.py
@@ -0,0 +1,29 @@
+import os
+
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch import receiver
+
+from .models import Recipe
+
+
+def delete_image(instance):
+    try:
+        os.remove(instance.image.path)
+    except (ValueError, FileNotFoundError) as e:
+        print(f"Error: {e}")
+
+
+@receiver(pre_delete, sender=Recipe)
+def recipe_image_delete(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    delete_image(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_image_update(sender, instance, raw, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    is_new_cover = old_instance.image != instance.image
+
+    if old_instance.image:
+        if is_new_cover:
+            delete_image(old_instance)
