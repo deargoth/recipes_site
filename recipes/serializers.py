5875d6b52f4a2e3f7a230e154e1b39a23823e24e
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-18 18:11:40 -0300

    Introdução ao DRF - Django Rest Framework

---
+++ recipes/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+
+from .models import Recipe
+from accounts.validators import RecipeValidatorForm
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = (
+            "title",
+            "description",
+            "slug",
+            "preparation_time",
+            "preparation_time_unit",
+            "servings",
+            "servings_unit",
+            "preparation_steps",
+            "image",
+            "preparation",
+            "servings_concat",
+            "tags",
+        )
+
+    preparation = serializers.SerializerMethodField(read_only=True)
+    servings_concat = serializers.SerializerMethodField(read_only=True)
+
+    def get_preparation(self, obj):
+        return f"{obj.preparation_time} {obj.preparation_time_unit}"
+
+    def get_servings_concat(self, obj):
+        return f"{obj.servings} {obj.servings_unit}"
+
+    def validate(self, attrs):
+        super_validate = super().validate(attrs)
+
+        RecipeValidatorForm(data=super_validate, ErrorClass=serializers.ValidationError)
+
+        return super_validate
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,16 +1,35 @@
 from django.urls import path
 from . import views
 
-app_name = 'recipes'
+app_name = "recipes"
 
-urlpatterns = [
-    path('', views.Index.as_view(), name="index"),
-    path('recipes/api/v1/', views.IndexApi.as_view(), name="index_api"),
-    path('recipes/<str:category_name>',
-         views.CategoriesPage.as_view(), name="category"),
-    path('recipes/details/<int:pk>', views.Details.as_view(), name="details"),
-    path('recipes/details/api/v1/<int:pk>',
-         views.DetailsApi.as_view(), name="details_api"),
-    path('recipes/search/', views.Search.as_view(), name="search"),
-    path('recipes/tags/<slug>', views.Tags.as_view(), name="tags"),
+
+views_urls = [
+    path("", views.Index.as_view(), name="index"),
+    path(
+        "recipes/<str:category_name>", views.CategoriesPage.as_view(), name="category"
+    ),
+    path("recipes/details/<int:pk>", views.Details.as_view(), name="details"),
+    path("recipes/search/", views.Search.as_view(), name="search"),
+    path("recipes/tags/<slug>", views.Tags.as_view(), name="tags"),
 ]
+
+api_urls = [
+    # V1 - Hard coded version - Understanding
+    path("recipes/api/v1/", views.IndexApi.as_view(), name="index_api"),
+    path(
+        "recipes/details/api/v1/<int:pk>",
+        views.DetailsApi.as_view(),
+        name="details_api",
+    ),
+    # V2 - Using Django Rest
+    path("recipes/api/v2/", views.recipes_api_v2_list, name="index_api_v2"),
+    path(
+        "recipes/details/api/v2/<int:pk>",
+        views.recipes_api_v2_details,
+        name="details_api_v2",
+    ),
+]
+
+
+urlpatterns = views_urls + api_urls
