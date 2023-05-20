0ba9ddd5b53137a800f3fb54d7756902640de7c0
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-20 12:09:39 -0300

    Usando generic views ao invés da view APIView

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,46 +1,14 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from ..models import Recipe
 from ..serializers import RecipeSerializer
 
 
-class RecipeAPIv2List(APIView):
-    recipes = Recipe.objects.filter(is_published=True)
-
-    def get(self, *args, **kwargs):
-        serializer = RecipeSerializer(instance=self.recipes, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, *args, **kwargs):
-        serializer = RecipeSerializer(data=self.request.data)
-        serializer.is_valid(raise_exception=True)
-
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.filter(is_published=True)
+    serializer_class = RecipeSerializer
 
 
-class RecipeAPIv2Details(APIView):
-    def setup(self, request, *args, **kwargs):
-        super().setup(request, *args, **kwargs)
-
-        pk = self.kwargs.get("pk", None)
-        self.recipe = Recipe.objects.filter(pk=pk).first()
-
-    def get(self, *args, **kwargs):
-        serializer = RecipeSerializer(instance=self.recipe, many=False)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def patch(self, *args, **kwargs):
-        serializer = RecipeSerializer(
-            instance=self.recipe, data=self.request.data, many=False, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-
-        serializer.save()
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def delete(self, *args, **kwargs):
-        self.recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RecipeAPIv2Details(RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.filter(is_published=True)
+    serializer_class = RecipeSerializer
