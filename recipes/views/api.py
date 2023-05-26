9b4661da3dd2d66872cae799501a17ba0856eb9d
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-26 19:15:11 -0300

    Fazendo verificações de permissões

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,8 +1,10 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
+from ..permissions import IsOwner
 from ..models import Recipe
 from ..serializers import RecipeSerializer
 
@@ -15,6 +17,17 @@ class RecipeAPIv2Viewset(ModelViewSet):
     queryset = Recipe.objects.filter(is_published=True)
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Paginator
+    http_method_names = [
+        "get",
+        "post",
+        "patch",
+        "delete",
+        "head",
+        "options",
+    ]
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+    ]
 
     def get_queryset(self):
         qs = super().get_queryset()
@@ -26,9 +39,33 @@ class RecipeAPIv2Viewset(ModelViewSet):
 
         return qs
 
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [
+                IsOwner(),
+            ]
+
+        return super().get_permissions()
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        obj = self.get_queryset().filter(pk=pk).first()
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
     def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get("pk")
-        recipe = self.get_queryset().filter(pk=pk).first()
+        recipe = self.get_object()
 
         serializer = RecipeSerializer(
             instance=recipe, data=request.data, many=False, partial=True
