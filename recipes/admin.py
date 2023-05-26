1ef7f0a7e183bfb1d9fd13e0c95af8883c17cd47
deargoth <viniciuseugeniovhe@gmail.com>
2023-05-26 19:08:41 -0300

    Adicionando list_select_related para remover as consultas desnecessárias do django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,14 +5,22 @@ from . import models
 
 @admin.register(models.Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'author',
-                    'created_at', 'updated_at', 'is_published')
+    list_display = (
+        "id",
+        "title",
+        "slug",
+        "author",
+        "created_at",
+        "updated_at",
+        "is_published",
+    )
 
-    list_display_links = ('id', 'title')
-    list_editable = ('is_published', )
+    list_display_links = ("id", "title")
+    list_editable = ("is_published",)
+    list_select_related = ("author",)
     # autocomplete_fields = ('tags', ),
     prepopulated_fields = {
-        'slug': ('title',),
+        "slug": ("title",),
     }
 
 
